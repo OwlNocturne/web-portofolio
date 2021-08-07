4b9cbca434654d9b1782c74ef1c09f4ccaeed14a
Dan <OwlNocturne@outlook.com>
2021-08-07 20:54:14 +0900

    Added Hero section component

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ import * as React from "react"
 import { ChakraProvider } from "@chakra-ui/react"
 import NavBar from "./components/NavBar"
 import theme from "./theme"
+import Hero from "./components/Hero"
 
 export const App = () => (
   <ChakraProvider theme={theme}>
     <NavBar />
+    <Hero />
   </ChakraProvider>
 )
---
+++ src/components/Hero.tsx
@@ -0,0 +1,47 @@
+import {
+    Stack,
+    Flex,
+    Button,
+    Text,
+    VStack,
+    useBreakpointValue,
+    Heading,
+} from '@chakra-ui/react';
+
+export default function Hero() {
+    return (
+        <Flex
+            w={'full'}
+            h={'40vh'}
+            backgroundImage={
+                'url(https://images.unsplash.com/photo-1556438064-2d7646166914?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1050&q=80)'
+            }
+            backgroundSize={'cover'}
+            backgroundPosition={'50% 65%'}>
+            <VStack
+                w={'full'}
+                justify={'center'}
+                px={useBreakpointValue({ base: 4, md: 8 })}
+                bgGradient={'linear(to-r, blackAlpha.800, transparent)'}>
+                <Stack w={'full'} align={'flex-start'} spacing={6}>
+                    <Heading
+                        fontWeight={600}
+                        fontSize={'6xl'}
+                        lineHeight={'110%'}
+                    >
+                        Title
+                    </Heading>
+                    <Text
+                        color={'white'}
+                        fontWeight={700}
+                        lineHeight={1.2}
+                        fontSize={"3xl"}
+                    >
+                        Description
+                    </Text>
+
+                </Stack>
+            </VStack>
+        </Flex>
+    )
+}
