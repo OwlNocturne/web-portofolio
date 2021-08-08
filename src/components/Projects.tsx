0f5aa4b22330ee84b8a33f5a17066db933360118
Dan <OwlNocturne@outlook.com>
2021-08-08 16:25:11 +0900

    Moved project data into it's a json.

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,38 +1,21 @@
+import React from "react";
 import { Center, Flex, Heading, SimpleGrid, VStack } from "@chakra-ui/react";
 import ProjectCard from "./ProjectCard";
 
-const project1 = {
-    imageUrl:
-        "https://assets.nintendo.com/image/upload/c_limit,f_auto,q_auto,w_1920/ncom/en_US/games/wii-u/e/eba-and-egg-a-hatch-trip-wii-u/screenshot-gallery/screenshot01.png",
-    imageAlt: "Eba Egg Logo",
-    title: "Eba & Egg",
-    shortDescription: "Wii U Game",
-    longDescription: "This is a game for the Wii U",
-    platforms: "Wii U",
-    languages: "C#",
-};
+import ProjectData from "../data/projects.json";
 
-const project2 = {
-    imageUrl: "https://jp.techcrunch.com/wp-content/uploads/2018/10/sub11.jpg",
-    imageAlt: "Sarah Hacker Club 1",
-    title: "Sarah Hacker Club",
-    shortDescription: "AR Game ",
-    longDescription: "This is an escape game for mobile games played in location",
-    platforms: "Android, iPhone",
-    languages: "C#",
-};
-
-const ProjectCards = [project1, project2];
+// const ProjectCards = ProjectData.;
 
 export default function Project() {
+    console.log(ProjectData);
     return (
         <Center w={"full"}>
             <VStack>
                 <Heading>Projects</Heading>
                 <Flex>
                     <SimpleGrid columns={2} spacing={10}>
-                        {ProjectCards.map((card) => (
-                            <ProjectCard project={card} />
+                        {ProjectData.map((data) => (
+                            <ProjectCard project={data} key={data.title} />
                         ))}
                     </SimpleGrid>
                 </Flex>
