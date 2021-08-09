706c5e8b7a2e5d65926f936a37361bf1dc5677ca
Dan <OwlNocturne@outlook.com>
2021-08-09 22:17:16 +0900

    Removed logo from company card. Visual changes to company card.

--- src/components/CompanyCard.tsx
+++ src/components/CompanyCard.tsx
@@ -1,18 +1,28 @@
-import { Box, Image } from "@chakra-ui/react";
+import { ExternalLinkIcon } from "@chakra-ui/icons";
+import { Box, Heading, Link, useColorModeValue } from "@chakra-ui/react";
 
 type CompanyCardProps = {
     name: string;
-    imageUrl: string;
-    imageAlt: string;
     websiteUrl: string;
 };
 
 export default function ProjectCard({ company }: { company: CompanyCardProps }) {
     return (
-        <Box maxW="sm" borderWidth="1px" borderRadius="lg" overflow="hidden" position="relative">
-            <Box display="block">
-                <Image src={company.imageUrl} alt={company.imageAlt} />
-            </Box>
+        <Box
+            display="block"
+            maxW={"320px"}
+            bg={useColorModeValue("white", "gray.900")}
+            boxShadow={"2xl"}
+            rounded={"lg"}
+            p={6}
+            textAlign={"center"}
+        >
+            <Heading fontSize={"2xl"} fontFamily={"body"}>
+                {company.name}
+            </Heading>
+            <Link href={company.websiteUrl} fontWeight={600} color={"gray.500"} mb={4} isExternal>
+                Corporate website <ExternalLinkIcon mx="2px" />
+            </Link>
         </Box>
     );
 }
