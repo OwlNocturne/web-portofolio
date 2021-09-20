bab73f922e6aac594603c71090b2d0ad735ae870
Dan <OwlNocturne@outlook.com>
2021-09-20 22:43:04 +0900

    Added localization support to portfolio section.

--- src/components/Portofolio.tsx
+++ src/components/Portofolio.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { Box, Center, Flex, Select, SimpleGrid, VStack } from "@chakra-ui/react";
 import ProjectCard from "./ProjectCard";
-import ProjectData from "../data/projects.json";
 import Seperator from "./Seperator";
 import { useTranslation } from "react-i18next";
 
 export default function Portofolio() {
     const [projectType, setProjectType] = React.useState(0);
     const { t } = useTranslation();
+    const projectData = t("projectsData", { returnObjects: true });
 
     return (
         <Center w={"full"} id="Portofolio">
@@ -20,15 +20,15 @@ export default function Portofolio() {
                         onChange={(e) => setProjectType(parseInt(e.target.value))}
                         mb={5}
                     >
-                        <option value={0}>All</option>
-                        <option value={1}>Games</option>
-                        <option value={2}>Apps</option>
-                        <option value={3}>Web</option>
+                        <option value={0}>{t("all")}</option>
+                        <option value={1}>{t("games")}</option>
+                        <option value={2}>{t("apps")}</option>
+                        <option value={3}>{t("web")}</option>
                     </Select>
                 </Box>
                 <Flex>
                     <SimpleGrid columns={3} spacing={10}>
-                        {ProjectData.map((data) => {
+                        {projectData.map((data) => {
                             if (projectType === 0 || data.projectType === projectType)
                                 return <ProjectCard project={data} key={data.title} />;
                             else return null;
